f68333b76a0cd229960e2d204add1dba1da7cb6b
Yunroug Gong <gongyunrou@gmail.com>
2017-10-28 18:39:00 -0400

    add eslinter config

--- treasure_hunter/src/main.js
+++ treasure_hunter/src/main.js
@@ -1,23 +1,22 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
-import Vue from 'vue'
-import App from './App'
-import router from './router'
-import VueLazyLoad from 'vue-lazyload'
+import Vue from 'vue';
+import VueLazyLoad from 'vue-lazyload';
 import infiniteScroll from 'vue-infinite-scroll';
+import App from './App.vue';
+import router from './router';
 
 Vue.config.productionTip = false;
 
 Vue.use(VueLazyLoad, {
-  loading:"/static/loading-svg/loading-bars.svg"
+  loading: '/static/loading-svg/loading-bars.svg',
 });
 
 Vue.use(infiniteScroll);
 
-/* eslint-disable no-new */
 new Vue({
   el: '#app',
   router,
   template: '<App/>',
-  components: { App }
+  components: { App },
 });
